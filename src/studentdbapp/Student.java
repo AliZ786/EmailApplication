b002da3b0924ece131a39553f0345cf3415d3f6c
AliZ786 <59709752+AliZ786@users.noreply.github.com>
2021-05-25 00:05:45 -0400

    Added courses enrolment method with balance updated

--- src/studentdbapp/Student.java
+++ src/studentdbapp/Student.java
@@ -12,11 +12,12 @@ public class Student {
     private String lName;
     private int currentYear;
     private String year;
-    private int StudentID;
+    private int yearNumber;
+    private String studentID;
     private String courses;
-    private String tuition;
-    private int COST = 600;
-
+    private int tuition = 0;
+    private static int COST = 600;
+    private static int id = 1000;
 
 
     public Student() {
@@ -28,35 +29,70 @@ public class Student {
         System.out.print("Enter student's last name: ");
         this.lName = input.nextLine();
 
-        System.out.println("Enter student's current grade level:\n1- Freshmen\n2-Sophomore\n3-Junior\n4-Senior");
+        System.out.println("Enter student's current grade level:\n1-Freshmen\n2-Sophomore\n3-Junior\n4-Senior");
         this.currentYear = input.nextInt();
 
-        switch(this.currentYear){
+        switch (this.currentYear) {
 
             case 1:
                 year = "Freshmen";
+                yearNumber = 1;
                 break;
 
             case 2:
                 year = "Sophomore";
+                yearNumber = 2;
                 break;
 
             case 3:
                 year = "Junior";
+                yearNumber = 3;
                 break;
 
             case 4:
                 year = "Senior";
+                yearNumber = 4;
                 break;
 
         }
-        System.out.println(fName + " " + lName +"\n" +year);
+
+        setStudentID();
+
+        System.out.println(fName + " " + lName + "\n" + "Current year: " + year + "\n" + "Student ID: " + studentID);
 
 
+    }
 
+    /**
+     * Method to generates an ID
+     */
+    private void setStudentID() {
+        id++;
+        this.studentID = yearNumber + "" + id;
+    }
 
+    /**
+     * Allows the student to enroll
+     */
 
+    public void enroll() {
 
+        do {
+            System.out.println("Enter the course that you wish to enroll in (Q to quit):");
+            Scanner in = new Scanner(System.in);
+            String courseChoice = in.nextLine();
+            if (!courseChoice.equals("Q")) {
+                courses = courses + "\n" + courseChoice;
+                tuition = tuition + COST;
 
+            } else {
+                System.out.println("Break!");
+                break;
+            }
+
+        } while (true);
+
+        System.out.println("Enrolled in: " +courses );
+        System.out.println("Tuition balance: " +tuition);
     }
 }
--- src/studentdbapp/StudentDBApp.java
+++ src/studentdbapp/StudentDBApp.java
@@ -4,6 +4,7 @@ public class StudentDBApp {
 
     public static void main(String args[]) {
         Student student1 = new Student();
+        student1.enroll();
 
     }
 }
